4d176bc00ed8577a4737920ee82ffcfed6695dca
kziarek <01169264@pw.edu.pl>
2023-12-19 14:09:26 +0100

    3b python works with python

--- 2/3b/Python/server/server.py
+++ 2/3b/Python/server/server.py
@@ -78,7 +78,7 @@ def serve(server_address, server_port):
 
 if __name__ == "__main__":
     parser = argparse.ArgumentParser(description='Script description')
-    parser.add_argument('--server_address', type=str, default='::1', help='Server address (IPv6)')
+    parser.add_argument('--server_address', type=str, default='z35_python_server_2_3', help='Server address (IPv6)')
     parser.add_argument('--server_port', type=int, default=8000, help='Server port')
 
     args = parser.parse_args()
